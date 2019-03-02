1db8686c07bfd5128802c62d43d98d98985408c3
Will Campbell <willcampbell444@gmail.com>
2019-03-02 15:57:49 -0500

    Basic task queue

---
+++ headers/taskQueue.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <queue>
+#include <list>
+#include <mutex>
+
+class Task {
+	friend class TaskQueue;
+
+	std::list<std::unique_ptr<Task>> after;
+
+public:
+	virtual void execute() = 0;
+	virtual ~Task() { }
+
+protected:
+	void runAfter(std::unique_ptr<Task> &&task) {
+		after.push_back(std::move(task));
+	}
+};
+
+class TaskQueue {
+	std::mutex lock;
+	std::queue<std::unique_ptr<Task>> tasks;
+
+	std::unique_ptr<Task> getFirst() {
+		std::lock_guard<std::mutex> l(lock);
+
+		if (tasks.empty()) {
+			return nullptr;
+		} else {
+			auto first = std::move(tasks.front());
+			tasks.pop();
+			return first;
+		}
+	}
+		
+public:
+	void insert(std::unique_ptr<Task> task) {
+		std::lock_guard<std::mutex> l(lock);
+
+		tasks.push(std::move(task));
+	}
+
+	bool runTask() {
+		auto task = getFirst();
+		if (task) {
+			task->execute();
+			for (auto& newTask : task->after) {
+				insert(std::move(newTask));
+			}
+			return true;
+		}
+
+		return false;
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
-#include <visualize.h>
+#include <memory>
+
+#include "visualize.h"
+#include "taskQueue.h"
+
+class RenderTask: public Task {
+	Visualize& m_visualize;
+
+public:
+	RenderTask(Visualize& v): m_visualize(v) { }
+	void execute() override;
+};
+
+class UpdateTask: public Task {
+	Visualize& m_visualize;
+
+public:
+	UpdateTask(Visualize& v): m_visualize(v) { }
+	void execute() override;
+};
+
+void RenderTask::execute() {
+	m_visualize.draw();
+	std::unique_ptr<Task> updateTask = std::make_unique<UpdateTask>(m_visualize);
+	runAfter(std::move(updateTask));
+}
+
+void UpdateTask::execute() {
+	m_visualize.update();
+	std::unique_ptr<Task> renderTask = std::make_unique<RenderTask>(m_visualize);
+	runAfter(std::move(renderTask));
+}
 
 int main() {
 	Visualize visualize;
-    while(!glfwWindowShouldClose(visualize.getWindow())) {
-        visualize.update();
-        visualize.draw();
+
+	TaskQueue taskQueue;
+	taskQueue.insert(std::make_unique<UpdateTask>(visualize));
+    while(!glfwWindowShouldClose(visualize.getWindow())
+		  && taskQueue.runTask()) {
+		  // ...
     }
 }
